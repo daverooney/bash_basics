09a89d383b899079858af74b8d325d5b1ae2c351
Dave Rooney <daverooney@users.noreply.github.com>
2020-02-27 12:27:02 -0600

    Update vimsetup.sh

--- vimsetup.sh
+++ vimsetup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
-sudo yum install git
+sudo yum install git tree
 mkdir -p ~/.vim/pack/plugins/start
+if [ -f ./_vimrc ]; then
+    cp ./_vimrc ~/.vimrc
+fi
 cd !$
 git clone https://github.com/saltstack/salt-vim.git salt.vim
 git clone https://github.com/Glench/Vim-Jinja2-Syntax.git jinja2.vim
 cd -
-if [ -f ./_vimrc ]; then
-    cp ./_vimrc ~/.vimrc
-fi
